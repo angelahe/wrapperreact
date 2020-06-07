65109ff82b9118e730f18e0e80bafb83e09d8fdf
angela <angela@aviak.ca>
2020-06-07 06:01:04 -0600

    create card wrapper component with children that can take any nested component

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
 import AnimalDetails from '../AnimalDetails/AnimalDetails';
 
 export default function AnimalCard({ name, size, ...props }) {
   return(
     <div>
-      <h3>{name}</h3>
-      <div>{size}kg</div>
-      <AnimalDetails
-        {...props}
-      />
+      <Card title="Animal">
+        <h3>{name}</h3>
+        <div>{size}kg</div>
+        <AnimalDetails
+          {...props}
+        />
+      </Card>
     </div>
   )
 }
