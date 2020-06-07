01d6edbec75804eb8065d652136e2b5b18833dd9
angela <angela@aviak.ca>
2020-06-07 05:43:35 -0600

    use spread to gather unused props in parent to pass to child

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -2,20 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AnimalDetails from '../AnimalDetails/AnimalDetails';
 
-export default function AnimalCard({ diet, name, size }) {
+export default function AnimalCard({ name, size, ...props }) {
   return(
     <div>
       <h3>{name}</h3>
       <div>{size}kg</div>
       <AnimalDetails
-        diet={diet}
+        {...props}
       />
     </div>
   )
 }
 
 AnimalCard.propTypes = {
-  diet: PropTypes.arrayOf(PropTypes.string).isRequired,
   name: PropTypes.string.isRequired,
   size: PropTypes.number.isRequired,
 }
